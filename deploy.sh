682fd0754a29aee02ef9b921d966eb7a5ee262d9
Ishwo Rai <38336613+ishwo@users.noreply.github.com>
2019-05-01 14:07:59 -0400

    Added deployment

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+docker build -t ishwo/multi-client:latest -t ishwo/multi_client:$SHA -f ./client/Dockerfile ./client
+docker build -t ishwo/multi-server:latest -t ishwo/multi_server:$SHA -f ./server/Dockerfile ./server 
+docker build -t ishwo/multi-worker:latest -t ishwo/multi-worker:$SHA -f ./worker/Dockerfile ./worker 
+docker push ishwo/multi-client:latest
+docker push ishwo/multi-server:latest
+docker push ishwo/multi-worker:latest
+docker push ishwo/multi-client:$SHA
+docker push ishwo/multi-server:$SHA
+docker push ishwo/multi-worker:$SHA
+
+kubectl apply -f k8s 
+kubectl set image deployments/server-deployment server=ishwo/multi-server:$SHA 
+kubectl set image deployments/client-deployment client=ishwo/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=ishwo/multi-server:$SHA
+
