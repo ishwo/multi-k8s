6cb708de83416777f07ae9ee1183da166fb28b23
Ishwo Rai <38336613+ishwo@users.noreply.github.com>
2019-05-01 14:47:00 -0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,15 @@
-docker build -t ishwo/multi-client:latest -t ishwo/multi_client:$SHA -f ./client/Dockerfile ./client
-docker build -t ishwo/multi-server:latest -t ishwo/multi_server:$SHA -f ./server/Dockerfile ./server 
-docker build -t ishwo/multi-worker:latest -t ishwo/multi-worker:$SHA -f ./worker/Dockerfile ./worker 
-docker push ishwo/multi-client:latest
-docker push ishwo/multi-server:latest
-docker push ishwo/multi-worker:latest
-docker push ishwo/multi-client:$SHA
-docker push ishwo/multi-server:$SHA
-docker push ishwo/multi-worker:$SHA
+docker build -t testmstr/multi-client:latest -t testmstr/multi_client:$SHA -f ./client/Dockerfile ./client
+docker build -t testmstr/multi-server:latest -t testmstr/multi_server:$SHA -f ./server/Dockerfile ./server 
+docker build -t testmstr/multi-worker:latest -t testmstr/multi-worker:$SHA -f ./worker/Dockerfile ./worker 
+docker push testmstr/multi-client:latest
+docker push testmstr/multi-server:latest
+docker push testmstr/multi-worker:latest
+docker push testmstr/multi-client:$SHA
+docker push testmstr/multi-server:$SHA
+docker push testmstr/multi-worker:$SHA
 
 kubectl apply -f k8s 
-kubectl set image deployments/server-deployment server=ishwo/multi-server:$SHA 
-kubectl set image deployments/client-deployment client=ishwo/multi-client:$SHA
-kubectl set image deployments/worker-deployment worker=ishwo/multi-server:$SHA
+kubectl set image deployments/server-deployment server=testmstr/multi-server:$SHA 
+kubectl set image deployments/client-deployment client=testmstr/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=testmstr/multi-server:$SHA
 
